ee077e198e69a977260eb77ed7607b65b3d93538
hong-seongmin <hong112424@naver.com>
2021-07-13 21:28:08 +0900

    6:20:00, asyncstorage를 이용해 다이어리 목록을 띄움

--- practice_SimpleDiary/pages/List.js
+++ practice_SimpleDiary/pages/List.js
@@ -3,6 +3,7 @@ import Container from '../components/Container';
 import Contents from '../components/Contents';
 import Sutton from '../components/Sutton';
 import styled from 'styled-components/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const ListItem = styled.TouchableOpacity`
     width:100%;
@@ -17,14 +18,37 @@ const Label = styled.Text`
 
 //구조 분해 할당, Destructuring Assignment
 function List({navigation}){
+    const [list, setList]  = React.useState( [] );
+
+    const load = async()=>{
+        const data = await AsyncStorage.getItem('list');
+        if (data !== null){
+            setList(JSON.parse(data));
+        }
+    }
+    
+    React.useEffect(() => {
+        const unsubscribe = navigation.addListener('focus', () => {//focus되면 데이터 가져옴 
+            load();
+        });
+
+        load();//개발도중의 refresh는 useEffect는 되고 focus는 안되기 때문에 목록이 안 보일 수 있기 때문에 목록이 잘 보이도록 함(개발 외의 경우에는 거의 일어나지 않음)
+    
+        return unsubscribe;
+      }, [navigation]);
     return(
         <Container>
             <Contents>
-                <ListItem onPress={()=>{
-                    navigation.navigate("Detail")//네비게이션에서 써야 하는 함수세트도 다 넘겨줌
-                }} >
-                    <Label>2021-07-12</Label>
-                </ListItem>
+                {list.map(item=>{
+                    return(
+                        <ListItem key={item.data} onPress={()=>{
+                            navigation.navigate("Detail")//네비게이션에서 써야 하는 함수세트도 다 넘겨줌
+                        }} >
+                            <Label>{item.date}</Label>
+                        </ListItem>
+                    )
+                })}
+
             </Contents>
             <Sutton onPress={()=>{
                 navigation.navigate('Form')
